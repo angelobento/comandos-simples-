c0c29979efc2e701fbd8129f8f2fe54cacbbeabb
angelobento <49735006+angelobento@users.noreply.github.com>
2019-08-25 18:02:02 -0300

    Add files via upload

---
+++ comando find.py
@@ -0,0 +1,26 @@
+frase =input("digite uma frase:")
+f = (input("o que quer encontrar?"))
+r = frase.find(f)
+if f == r:
+    print("{} existe na sua frase".format(f))
+
+#esse comando não terminado tem por objetiva achar uma palavra dentro de
+#uma frase, não consegui fazer dar certo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
