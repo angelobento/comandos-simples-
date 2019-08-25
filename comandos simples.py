38e5e25ebd81c62de75aecc65104852960d05cf2
angelobento <49735006+angelobento@users.noreply.github.com>
2019-08-25 17:11:34 -0300

    Add files via upload
    comando para contar consaantes e vogais

---
+++ comandos simples.py
@@ -0,0 +1,60 @@
+frase = (input("digite uma frase:"))#conta quantas vogal e consuantes tem na string
+v=frase.count("a")
+o=frase.count("e")
+g=frase.count("i")
+a=frase.count("o")
+l=frase.count("u")
+vogal = v +o+g+a+l
+b=frase.count("b")
+c=frase.count("c")
+d=frase.count("d")
+f=frase.count("f")
+G=frase.count("g")
+h=frase.count("h")
+j=frase.count("j")
+k=frase.count("k")
+l=frase.count("l")
+m=frase.count("m")
+n=frase.count("n")
+p=frase.count("p")
+q=frase.count("q")
+r=frase.count("r")
+s=frase.count("s")
+t=frase.count("t")
+V=frase.count("v")
+w=frase.count("w")
+x=frase.count("x")
+y=frase.count("y")
+z=frase.count("z")
+consuantes= b+c+d+f+G+h+j+k+l+m+n+p+q+r+s+t+V+w+x+y+z
+#print("sua frase tem {} letras A, {} letras E, {} letras I, {} letras o e {} letras U".format(v,o,g,a,l))
+#print ("sua frase tem {} vogal".format(vogal))
+print("sua frase tem {} vogal e {} consuantes".format(vogal, consuantes))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
