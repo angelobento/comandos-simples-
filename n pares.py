082a1dc06960f2886684c4a435c1214403ad388d
angelobento <49735006+angelobento@users.noreply.github.com>
2019-09-10 17:29:38 -0300

    Add files via upload

---
+++ n pares.py
@@ -0,0 +1,40 @@
+# from time import sleep
+# for k in range (10, -1, -1):
+#     print(k)
+#     sleep(1)
+# print ("bommm")
+#contagem regressiva
+
+#############################################################################
+# for k in range (2, 51, 2):
+#     print(k, end =' ')
+#conta de 2 ate 50 somente os numeros pares
+# cont = 0
+# np = int(input("digite um numero par: "))
+# ni = int(input("digite um numero impar: "))
+# for k in range (np, ni, 2):
+#     print(k)
+#     cont += k
+# print("de {} ate {} os numeros pares são {}". format (np, ni, cont))
+##########################################################################
+# soma = 0
+# for k in range(1, 501, 2):
+#     if k % 3 == 0:
+#         soma+=k
+# print("a soma dos numeros divisiveis por 3 de 0 ate 500 é {}".format (soma))
+#########################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
