bffd2833ceee0238406629510bd3067d26c141d4
angelobento <49735006+angelobento@users.noreply.github.com>
2019-09-10 17:27:23 -0300

    Add files via upload

---
+++ progressão aritimética.py
@@ -0,0 +1,36 @@
+inicio = int(input("início:"))
+razao = int(input("razão:"))
+dez = inicio + (10 - 1) * razao
+for k in range(inicio, dez + razao, razao):
+    print("{}".format(k), end= '-> ')
+
+#esse programa mostra os 10 procimos números pulando de quantos voce deinir 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ soma.py
@@ -0,0 +1,41 @@
+s = 0
+for k in range(0, 3):
+    n = int(input("digite um numero:"))
+    s += n
+print(" a soma é:{}".format(s))
+
+#esse programa mostra a soma de 3 input
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
