aca336d5c71a471bb56836b1ca55f805634caccb
angelobento <49735006+angelobento@users.noreply.github.com>
2019-09-27 20:21:16 -0300

    Add files via upload

---
+++ correção p.py
@@ -0,0 +1,65 @@
+#imprima com o po na frente
+# s = "a casa branca"
+# n=len(s)
+# ns = "P"
+# for k in range (n):
+#     if s[k]==" ":
+#         ns=ns+" P"
+#     else:
+#         ns=ns+s[k]
+#     print(k,ns)
+
+
+s = "10 de outubro de 2019"
+res = "10/01/2019"
+n=len(s)
+if s[6:9]=="jan":
+    print(s[0:2]+"/01/"+s[n-4:n])
+if s[6:9]=="fev":
+    print(s[0:2]+"/02/"+s[n-4:n])
+if s[6:9]=="mar":
+    print(s[0:2]+"/03/"+s[n-4:n])
+if s[6:9]=="abr":
+    print(s[0:2]+"/04/"+s[n-4:n])
+if s[6:9]=="mai":
+    print(s[0:2]+"/05/"+s[n-4:n])
+if s[6:9]=="jun":
+    print(s[0:2]+"/06/"+s[n-4:n])
+if s[6:9]=="jul":
+    print(s[0:2]+"/07/"+s[n-4:n])
+if s[6:9]=="ago":
+    print(s[0:2]+"/08/"+s[n-4:n])
+if s[6:9]=="set":
+    print(s[0:2]+"/09/"+s[n-4:n])
+if s[6:9]=="out":
+    print(s[0:2]+"/10/"+s[n-4:n])
+if s[6:9]=="nov":
+    print(s[0:2]+"/11/"+s[n-4:n])
+if s[6:9]=="dez":
+    print(s[0:2]+"/12/"+s[n-4:n])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
