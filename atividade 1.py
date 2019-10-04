594eebc4eb3eda954c4556763265ce553b8cb089
angelobento <49735006+angelobento@users.noreply.github.com>
2019-10-04 20:45:33 -0300

    Add files via upload

---
+++ atividade 1.py
@@ -0,0 +1,26 @@
+s = 'o carro russo'
+for k in range (s):
+    if s REPITIR A LETRA NA PALAVRA:
+        colocar uma "," no meio das letras repitidas
+    else:segue a vida normal
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ atv2.py
@@ -0,0 +1,43 @@
+d = 'dia 06 de janeiro'
+dia = d[4:6]
+mes = d[10:13]
+if mes == "jan":
+    print("dia {} de {}".format(dia, mes))
+if mes == "fev":
+    print("dia {} de {}".format(dia, mes))
+if mes == "mar":
+    print("dia {} de {}".format(dia, mes))
+if mes == "abr":
+    print("dia {} de {}".format(dia, mes))
+if mes == "mai":
+    print("dia {} de {}".format(dia, mes))
+if mes == "jun":
+    print("dia {} de {}".format(dia, mes))
+if mes == "jul":
+    print("dia {} de {}".format(dia, mes))
+if mes == "ago":
+    print("dia {} de {}".format(dia, mes))
+if mes == "set":
+    print("dia {} de {}".format(dia, mes))
+if mes == "out":
+    print("dia {} de {}".format(dia, mes))
+if mes == "nov":
+    print("dia {} de {}".format(dia, mes))
+if mes == "des":
+    print("dia {} de {}".format(dia, mes))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
